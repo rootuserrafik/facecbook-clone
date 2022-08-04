54e15ba224433d9f1b60fc7d8ce2b5e97d6a3135
Md. Rafikul Islam <contatwithrafik@gmail.com>
2022-08-04 12:44:40 +0600

    first commit

---
+++ build/precache-manifest.eacdb3b0c362d8192ef1eb2b0ef855ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "304baf5463cb5c9a52aebed728a8cf2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4defc0c6b29b90c86330",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "20434e3de9cb8a370d1c",
+    "url": "/static/js/2.00d32eec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00d32eec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4defc0c6b29b90c86330",
+    "url": "/static/js/main.31e8a684.chunk.js"
+  },
+  {
+    "revision": "b22f39492ac22e50809c",
+    "url": "/static/js/runtime-main.2978457e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
